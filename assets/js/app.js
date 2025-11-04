16afc88392b8c2e90d13f006f1cb05cabb9f9b3c
bo-nicolaisen <bo@squirrelworks.dk>
2025-11-04 09:18:52 +0100

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// #region serviceworker registration
+
+if ("serviceWorker" in navigator) {
+  
+  navigator.serviceWorker.register("sw.js")
+  
+  .then(registration => {
+    console.log("sw registered");
+ 
+
+  }).catch(error => {
+    console.log("sw reg fail");
+    console.log(error);
+
+  })
+
+} else {
+
+//browser does not support service worker
+  alert("pwa not supported");
+  /* do non pwa stuff here*/
+}
+
+// #endregion serviceworker registration
